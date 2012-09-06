cd426f03060ee022a67948bcf7d73b2a80114372
kevinjseo91 <kevinjseo91@gmail.com>
2012-09-06 11:15:34 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Susiee::Application.config.secret_token = '317f3f94fa443dfa2db912e323a3d164aae429f99b249248513762c64c71d6ec260bc601758e574832341e15d65779e3598753465afd9785f6aa11f928b7cc4c'
